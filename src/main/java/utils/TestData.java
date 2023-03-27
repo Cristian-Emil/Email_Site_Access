7d5c542c879a36fa29aa3495536fa7c3437659a6
Cristian Zidarescu <124686433+Cristian-Emil@users.noreply.github.com>
2023-03-27 09:56:10 +0300

    Update TestData.java

--- src/main/java/utils/TestData.java
+++ src/main/java/utils/TestData.java
@@ -5,12 +5,12 @@ public class TestData {
     public final static String APP_URL = "https://outlook.com" ;
 //    public final static String APP_URL = "https://www.hotmail.com" ;
 
-//    public final static String ADMIN_USERNAME = "cristian-testare@outlook.com" ;
+    public final static String ADMIN_USERNAME = "cristian-testare@outlook.com" ;
 
-    public final static String ADMIN_USERNAME = "hardsoft21@hotmail.com" ;
+//    public final static String ADMIN_USERNAME = "emailaddress@hotmail.com" ;
 
-//    public final static String ADMIN_PASSWORD = "Cristian21-test" ;
+    public final static String ADMIN_PASSWORD = "Cristian21-test" ;
 
-    public final static String ADMIN_PASSWORD = "8910Hardsoft" ;
+//    public final static String ADMIN_PASSWORD = "password" ;
 
 }
